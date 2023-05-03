9fa0654d11d595df5bea7e90da48d9b2e6fecef5
Debbie Kabir <124077394+DebK76@users.noreply.github.com>
2023-05-02 22:25:56 -0400

    commit updates

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,41 +1,78 @@
 import os
 import csv
-csvpath ="C:/Users/debbi/OneDrive/Documents/GitHub/python-challenge/Resources/budget_data.csv"
+csvpath = "../Resources/budget_data.csv"
 
 with open(csvpath) as csvfile:
-
+    
     # CSV reader specifies delimiter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter=',')
-
     print(csvreader)
 
-    # Read the header row first (skip this step if there is now header)
+    # Read the header row first (skip this step if there is no header)
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
 
-    # Read each row of data after the header
-    sum_months = 0
+     # Initialize variables
+    total_months = 0
+    net_profit_loss = 0
+    changes = []
+    max_increase = ['', 0]
+    max_decrease = ['', 0]
+    prev_month_profit_loss = None
+
+    # Loop through each row in the CSV file
     for row in csvreader:
-        #print(f"date = {row[0]}") 
-        #print(f"profit/losses = {row[1]}") 
-        # adding 1 for reach row  to the sum_months number assuming each row is a month
-        sum_months = sum_months + 1
-        print(f"total number of months = {sum_months}")
+        date = row[0]
+        profit_loss = int(row[1])
+        total_months += 1
+        net_profit_loss += profit_loss
 
-        #Question 2
+        # Calculate change in profit/loss from previous month
+        if prev_month_profit_loss is not None:
+            change = profit_loss - prev_month_profit_loss
+            changes.append(change)
+            if change > max_increase[1]:
+                max_increase = [date, change]
+            elif change < max_decrease[1]:
+                max_decrease = [date, change]
+        prev_month_profit_loss = profit_loss
 
-        with open(csvpath) as csvfile:
+    # Calculate average change in profit/loss
+    avg_change = sum(changes) / len(changes)
 
-    # CSV reader specifies delimiter and variable that holds contents
-            csvreader = csv.reader(csvfile, delimiter=',')
+    # Print the results
+    print(f'Total Months: {total_months}')
+    print(f'Total: ${net_profit_loss}')
+    print(f'Average Change: ${avg_change:.2f}')
+    print(f'Greatest Increase in Profits: {max_increase[0]} (${max_increase[1]})')
+    print(f'Greatest Decrease in Profits: {max_decrease[0]} (${max_decrease[1]})')
+    
 
-    print(csvreader)
+    print ("Financial Analysis")
+    print ("--------------------------")
+    print(total_months)
+    print(profit_loss)
+    print(change)
+    print(len(changes))
+    print(avg_change)
+    print(f'Greatest Increase in Profits: {max_increase[0]} (${max_increase[1]})')
+    print(f'Greatest Decrease in Profits: {max_decrease[0]} (${max_decrease[1]})')
+       
 
-    # Read the header row first (skip this step if there is now header)
-    csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
-    total_profit_loss = 0
-    #converting profit/loss string to an integer
-    print(f"date = {row[0]}")
-    print(f"the net total profit/losses = {row[1]}")
-   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
